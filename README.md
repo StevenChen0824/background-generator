3f326fd17ddce9509db57ba1127063855dbb6bca
Steven <stevenchen841204@gmail.com>
2024-10-14 21:48:03 +0800

    update readme and add comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # background-generator
+## Tech
+HTML, CSS, Javascript
+## Main Goal
+Practice using Javascript to
+1. Let user interact with website.
+2. Select element for website.
+3. Change style for website.
--- script.js
+++ script.js
@@ -1,33 +1,41 @@
+// select elements
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var css = document.querySelector("h3");
+var h3 = document.querySelector("h3");
 var body = document.getElementById("gradient");
 var button = document.querySelector(".colorR");
 
-function setgradient() {
+// set linear background with current color
+function setGradient() {
 	body.style.background = "linear-gradient(to right," 
 	+ color1.value 
 	+ "," 
 	+ color2.value 
 	+ ")";
-	css.textContent = body.style.background + ";";
+	h3.textContent = body.style.background + ";";
 }
 
+// generate random color for color1
 function getRandomColor1() {
 	var letters = "0123456789abcdef";
 	var color = "#"; 
 	for(var i = 0; i < 6; i++ ) {
 		color += letters[Math.floor(Math.random()*16)];
-	} 
+	}
+	// set input color1 to random color
+	color1.value = color;
 	return color;
 }
 
+// generate random color for color2
 function getRandomColor2() {
 	var letters = "0123456789abcdef";
 	var color = "#"; 
 	for(var i = 0; i < 6; i++ ) {
 		color += letters[Math.floor(Math.random()*16)];
 	} 
+	// set input color2 to random color
+	color2.value = color;
 	return color;
 }
 
@@ -35,14 +43,14 @@ function setRandomGradient() {
 	var r1 = getRandomColor1();
 	var r2 = getRandomColor2();
 	body.style.background = "linear-gradient(to right," + r1 + "," + r2 + ")";
-	css.textContent = body.style.background + ";";
+	h3.textContent = body.style.background + ";";
 }
 
-setgradient();
+setGradient();
 
-color1.addEventListener("input", setgradient);
+color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setgradient);
+color2.addEventListener("input", setGradient);
 
 button.addEventListener("click", setRandomGradient);
 
